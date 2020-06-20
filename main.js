d81cb116fbd04cf9985ea63f37b8d7ab88d1ffe5
Vladimir Safonov <Vladimir.Safonov@kaspi.kz>
2020-06-20 19:27:13 +0600

    Login process ready

--- main.js
+++ main.js
@@ -1,28 +1,36 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, ipcMain, dialog } = require('electron');
 const url = require('url');
 const path = require('path');
-const store = new (require('electron-store'))();
 const constants = require('./appConfig');
 
-store.set('value', 'am a constant');
-
-function createWindow () {
-    // Create the browser window.
-    const win = new BrowserWindow({
-        width: 600,
-        height: 525,
-        icon: path.join(constants.imgDir, 'icon.png'),
+let loginWindow;
+function createLoginWindow() {
+    loginWindow = new BrowserWindow({
+        width: 800,//440
+        height: 600,//225
+        resizable: false,
+        movable: false,
+        center: true,
+        backgroundColor: '#222233',
+        alwaysOnTop: false,
+        icon: path.join(constants.imgResDir, 'icon.png'),
         webPreferences: {
             nodeIntegration: true
-        }
+        },
     });
 
-  win.loadFile(path.join(constants.viewsDir, 'login.html'));
+    loginWindow.removeMenu();
 
-  win.webContents.openDevTools();
+    loginWindow.loadFile(path.join(constants.viewsDir, 'login.html'));
+
+    loginWindow.once('ready-to-show', () => {
+        loginWindow.show();
+    });
+
+    loginWindow.webContents.openDevTools();
 }
 
-app.whenReady().then(createWindow);
+app.whenReady().then(createLoginWindow);
 
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
@@ -32,6 +40,54 @@ app.on('window-all-closed', () => {
 
 app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
-        createWindow();
+        createLoginWindow();
     }
 });
+
+let adConfig;
+ipcMain.on('create-ad-config', (event, config) => {
+    adConfig = {
+        url: config.url,
+        baseDN: config.baseDN,
+        user: config.username,
+        pass: config.password,
+        tlsOptions: config.tlsOptions
+    };
+});
+
+ipcMain.on('open-error-dialog', (event, ...message) => {
+    dialog.showErrorBox(message[0], message[1]);
+});
+
+ipcMain.on('give-ad-object', (event) => {
+    const AD = require('ad');
+    const ad = new AD(adConfig);
+    event.reply('take-ad-object', ad);
+});
+
+ipcMain.on('authentication-success', (event) => {
+    console.log('Authentication success');
+    createUsersWindow();
+    loginWindow.close();
+    event.returnValue = 0;
+});
+
+function createUsersWindow() {
+    const usersWindow = new BrowserWindow({
+        width: 800,//440
+        height: 600,//225
+        icon: path.join(constants.imgResDir, 'icon.png'),
+        webPreferences: {
+            nodeIntegration: true
+        },
+    });
+
+    usersWindow.loadFile(path.join(constants.viewsDir, 'users.html'));
+
+    usersWindow.once('ready-to-show', () => {
+        usersWindow.show();
+    });
+
+    usersWindow.webContents.openDevTools();
+}
+
--- models/login.js
+++ models/login.js
@@ -1,47 +1,72 @@
-const AD = require('ad');
-const constants = require('../../appConfig.js');
+const { ipcRenderer } = require('electron');
+const ActiveDirectory = require('activedirectory');
+const constants = require('../appConfig.js');
+const store = new (require('electron-store'))();
 
 let submit = document.querySelector('#login-submit');
 
-submit.onclick = async () => {
-    let loginUsername = document.querySelector('#login-username');
-    let loginPassword = document.querySelector('#login-password');
+submit.onclick = () => {
+    const loginUrl = document.querySelector('#login-url');
+    const loginBaseDN = document.querySelector('#login-basedn');
+    const loginUsername = document.querySelector('#login-username');
+    const loginPassword = document.querySelector('#login-password');
+    const loginRow = document.querySelector('#login-row');
+
     if (loginUsername.value === '') {
-        alert('Username is required');
+        ipcRenderer.send('open-error-dialog', 'Username error', 'Username is required');
+        return false;
+    } else if (loginPassword.value === '') {
+        ipcRenderer.send('open-error-dialog', 'Password error', 'Password is required');
         return false;
     }
 
-    if (loginPassword.value === '') {
-        alert('Password is required');
-        return false;
-    }
-    try {
-        const ad = new AD({
-            url: constants.url,
-            baseDN: constants.baseDN,
-            user: loginUsername.value + constants.postfix,
-            pass: loginPassword.value
-        });
+    store.set('loginUrl', loginUrl.value);
+    store.set('loginBaseDN', loginBaseDN.value);
+    store.set('loginUsername', loginUsername.value + constants.postfix);
 
-        let authenticated = await authenticate(ad, loginUsername.value, loginPassword.value)
-            .then(result => {
-                return result === true ? true : false;
-            })
-            .catch(() => false);
-
-        if (authenticated === true) {
-            return true;
+    const config = {
+        url: loginUrl.value,
+        baseDN: loginBaseDN.value,
+        password: loginPassword.value,
+        username: loginUsername.value + constants.postfix,
+        tlsOptions: {
+            requestCert: true,
+            rejectUnauthorized: false,
         }
+    };
 
-        console.log(authenticated);
-        return false;
-    } catch (e) {
-        alert(e.message);
-        return false;
-    }
+    const ad = new ActiveDirectory(config);
 
+    ad.authenticate(config.username, config.password, function (err, auth) {
+        if (auth === true) {
+            ipcRenderer.send('create-ad-config', config);
+            ipcRenderer.sendSync('authentication-success');
+        } else {
+            ipcRenderer.send('open-error-dialog', 'Login error', 'Authentication failed');
+            loginPassword.value = '';
+            console.log(JSON.stringify(err, null, 4));
+        }
+    });
 };
 
-function authenticate(ad, username, password) {
-    return ad.user(username).authenticate(password);
+document.addEventListener('DOMContentLoaded', () => {
+    const url = document.querySelector('#login-url');
+    const basedn = document.querySelector('#login-basedn');
+    const user = document.querySelector('#login-username');
+    const password = document.querySelector('#login-password');
+
+    url.value = constants.url || store.get('loginUrl') || '';
+    basedn.value = constants.baseDN || store.get('loginBaseDN') || '';
+    user.value = constants.username || store.get('loginUsername') || '';
+    password.value = constants.password || '';
+});
+
+function showCredentialsErrorMessage(element, credential) {
+    element.insertAdjacentHTML('afterend', `<div id="login-error" class="alert alert-danger"><span>${credential} is required</span></div>`);
+    setTimeout(() => document.querySelector('#login-error').hidden = true, 3000);
+}
+
+function showAuthErrorMessage(element) {
+    element.insertAdjacentHTML('afterend', '<div id="login-error" class="alert alert-danger"><span>Authentication failed</span></div>');
+    setTimeout(() => document.querySelector('#login-error').hidden = true, 3000);
 }
